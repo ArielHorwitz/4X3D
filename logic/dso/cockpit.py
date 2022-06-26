60302fe52c0ad1cf6bdf8cc9247b9838a2f8393b
Ariel Horwitz <_@_._>
2022-06-26 19:20:21 +0300

    Add support for multiple labels per object in CharMap, add distance labelfrom cockpit

--- logic/dso/cockpit.py
+++ logic/dso/cockpit.py
@@ -1,12 +1,13 @@
 from loguru import logger
+import math
 import numpy as np
 from functools import partial
 
 from util.config import CONFIG_DATA
-from util import OBJECT_COLORS, CELESTIAL_NAMES
+from util import OBJECT_COLORS, CELESTIAL_NAMES, EPSILON
 from util.argparse import arg_validation
 from util.camera import Camera
-from util.charmap import CharMap
+from util.charmap import CharMap, TooSmallError
 
 
 
@@ -15,6 +16,10 @@ class Cockpit:
         self.ship = ship
         self.camera = Camera()
         self.show_labels = CONFIG_DATA['SHOW_LABELS']
+        self.show_dbar = CONFIG_DATA['SHOW_DISTANCE_BARS']
+        self._dbar_size = CONFIG_DATA['DISTANCE_BARS_SIZE']
+        self._dbar_maxd = CONFIG_DATA['DISTANCE_BARS_MAXD']
+        self._dbar_curve = CONFIG_DATA['DISTANCE_BARS_CURVE']
         self.camera_following = None
         self.camera_tracking = None
         self._last_charmap_state = bytes(1)
@@ -29,6 +34,10 @@ class Cockpit:
             ('pro', self.look_prograde),
             ('retro', self.look_retrograde),
             ('labels', self.toggle_labels),
+            ('dbar', self.toggle_dbar),
+            ('dbar.size', self.set_dbar_size),
+            ('dbar.maxd', self.set_dbar_maxd),
+            ('dbar.curve', self.set_dbar_curve),
         ]
         return cockpit_commands + self.camera.commands
 
@@ -106,35 +115,96 @@ class Cockpit:
 
     def toggle_labels(self):
         """Toggle labels"""
-        self.show_labels = (self.show_labels + 1) % 4
+        self.show_labels = (self.show_labels + 1) % 3
+
+    def toggle_dbar(self):
+        """Toggle distance bar"""
+        self.show_dbar = (self.show_dbar + 1) % 4
+
+    def set_dbar_size(self, size):
+        """ArgSpec
+        Set the maximum size of the distance bar.
+        ___
+        +SIZE An integer greater than 0
+        """
+        with arg_validation(f'Max distance must be an integer greater than 0'):
+            size = int(size)
+            assert size > 0
+        self._dbar_size = size
+
+    def set_dbar_maxd(self, maxd):
+        """ArgSpec
+        Set the maximum distance discernable by the distance bar.
+        ___
+        +MAXD A number greater than 0
+        """
+        with arg_validation(f'Max distance must be a number greater than 0'):
+            maxd = float(maxd)
+            assert maxd > 0
+        self._dbar_maxd = float(maxd)
+
+    def set_dbar_curve(self, curve):
+        """ArgSpec
+        Set the curve at which the distance bar shrinks with distance.
+        ___
+        +CURVE An integer bewteen 0 and 1
+        """
+        with arg_validation(f'Curve must be a number between 0 and 1'):
+            curve = float(curve)
+            assert 0 <= curve
+        self._dbar_curve = curve
 
     # Display
     def draw_charmap(self, size):
-        if size[0] < CharMap.MINIMUM_SIZE or size[1] < CharMap.MINIMUM_SIZE:
-            return 'Window too small'
-        charmap = CharMap(self.camera, size)
-        points = self.universe.positions
-        label_getter = self.get_label if self.show_labels else None
+        try:
+            charmap = CharMap(self.camera, size)
+        except TooSmallError as e:
+            return f'{e}'
+        # Collect labels
+        # Since we are using add_objects (plural), we must pass a getter
+        # (get_labels) for the labels, which in turn returns a sequence of
+        # labels for a particular oid. We rely heavily on locals to remember
+        # relevant information for this frame.
+        local_label_getters = []
+        if self.show_labels:
+            # This is simple and doesn't not require any specific state
+            local_label_getters.append(self.get_label)
+        if self.show_dbar:
+            # This is more complex and requires a new function def with its own
+            # locals to refer to distances and their deltas for this particular
+            # frame.
+            local_label_getters.append(self.get_dist_label_getter())
+        def get_labels(oid):
+            # The actual labels getter for a particular oid. Simply get
+            # a label from each getter in this frames label_getters list.
+            return tuple(getter(oid) for getter in local_label_getters)
+        # If we have labels to show, use the local get_labels
+        get_labels = get_labels if local_label_getters else None
+        # Finally, add the objects
         charmap.add_objects(
-            points=points,
+            points=self.universe.positions,
             icon=self.get_icon,
             tag=self.get_tag,
-            label=label_getter,
+            labels=get_labels,
         )
-        charmap.add_projection_axes()
+        # Add other elements
+        if self.show_labels:
+            charmap.add_projection_axes()
         charmap.add_crosshair()
         charmap.add_prograde_retrograde(
             velocity=self.ship.velocity,
             show_labels=self.show_labels,
             show_speed=self.show_labels > 1,
         )
-        return charmap.draw()
+        final_string = charmap.draw()
+        return final_string
 
     def get_charmap(self, size):
         state = ''.join([
             str(self.universe.tick),
             str(size),
             str(self.show_labels),
+            str(self.show_dbar),
             str(self.camera.state),
         ])
         current_state = bytes(state, encoding='utf-8')
@@ -152,11 +222,29 @@ class Cockpit:
     def get_label(self, oid):
         ob = self.universe.ds_objects[oid]
         lbl = ''
-        if self.show_labels == 1:
-            lbl = f'{ob.oid}'
-        elif self.show_labels >= 2:
-            lbl = f'{ob.oid}.{ob.name}'
-        if self.show_labels == 3:
-            dist = np.linalg.norm(self.camera.pos - ob.position)
-            lbl = f'{lbl} ({dist:.3e})'
-        return lbl
+        lbl = f'{ob.oid}'
+        if self.show_labels >= 2:
+            lbl = f'{lbl}.{ob.name}'
+        return lbl, None
+
+    def get_dist_label_getter(self):
+        rel_pos = self.universe.positions-self.ship.position
+        rel_pos2 = self.universe.positions+self.universe.velocities-(self.ship.position+self.ship.velocity)
+        distances = np.linalg.norm(rel_pos, axis=-1)
+        distances_step = np.linalg.norm(rel_pos2, axis=-1)
+        stationary = np.abs(distances - distances_step) < EPSILON
+        redshift = distances < distances_step
+        def get_dist_label(oid):
+            dfactor = max(0, min(distances[oid] / self._dbar_maxd, 1))
+            curve = math.log2(dfactor+1) ** self._dbar_curve
+            bar_size = self._dbar_size - min(self._dbar_size-1, round(curve * self._dbar_size))
+            bar = '─' * bar_size
+            tag = None if stationary[oid] else 'red' if redshift[oid] else 'blue'
+            if self.show_dbar == 1:
+                bar = f'{bar}'
+            elif self.show_dbar == 2:
+                bar = f'{distances[oid]:.2e}'
+            elif self.show_dbar == 3:
+                bar = f'{bar} {distances[oid]:.2e}'
+            return bar, tag
+        return get_dist_label
--- util/charmap.py
+++ util/charmap.py
@@ -9,30 +9,48 @@ from util.config import CONFIG_DATA
 WHITESPACE = '<whitespace> </whitespace>'
 
 
-class CharMap:
-    MINIMUM_SIZE = 3
+class TooSmallError(Exception):
+    pass
 
+
+class CharMap:
+    """Character Map
+
+    This class uses a camera to project points in 3D space to a 2D map of
+    characters (multiline string), essentially acting as a window to a Camera
+    object.
+
+    To use, initialize the character map with POV camera and window size.
+    Add elements to the character map (such as objects or crosshairs).
+    Finally, call the draw() method to get a string ready for printing.
+
+    A TooSmallError will be raised if the requested size is too small.
+    """
     def __init__(self, camera, size, show_bar=True, minimum_label_size=4):
         self.camera = camera
         self.width, self.height = size
-        self.minimum_label_size = minimum_label_size
         self.show_bar = show_bar
+        self.size = self.width, self.height
+        # Things get wonky if there isn't enough room for a crosshair (3x3)
+        # Also consider that the bar will take up one row
+        self.minimum_size = 3, 3 + self.show_bar
+        if self.width < self.minimum_size[0] or self.height < self.minimum_size[1]:
+            raise TooSmallError(f'CharMap too small:\nRequested {self.size} minimum {self.minimum_size}')
         if self.show_bar:
             self.height -= 1
-        self.size = self.width, self.height
-        if self.width < self.MINIMUM_SIZE or self.height < self.MINIMUM_SIZE:
-            raise ValueError(f'CharMap size too small: {self.size} (minimum: {self.MINIMUM_SIZE})')
+        self.minimum_label_size = minimum_label_size
         self.center = self.width // 2, self.height // 2
         self.charmap = [[' '] * self.width for _ in range(self.height)]
         self.camera.update()
 
+    # Drawing
     def draw(self):
         map_str = '\n'.join(''.join(_) for _ in self.charmap)
         if self.show_bar:
-            map_str = f'<map>{map_str}</map>\n<bar>{self.get_bar()}</bar>'
+            map_str = f'<map>{map_str}</map>\n<bar>{self.__get_bar()}</bar>'
         return map_str
 
-    def get_bar(self):
+    def __get_bar(self):
         following = '<grey>FLW</grey>' if self.camera.following is None else '<h2>FLW</h2>'
         tracking = '<grey>TRK</grey>' if self.camera.tracking is None else '<h2>TRK</h2>'
         camera_str = f'{following} {tracking}'
@@ -44,29 +62,58 @@ class CharMap:
             f'<code>{self.width}×{self.height}</code>',
         ])
 
-    def add_objects(self, points, icon, tag, label=None):
-        pix_pos = self.get_projected_pixels(points)
-        labels = []
+    # Map elements
+    def add_objects(self, points, icon, tag, labels=None):
+        """Add a list of objects to the character map.
+
+        Similar to add_object(), however icon, tag, and labels are given as
+        functions that take an index (of the point from points) and return its
+        respective icon, tag, and labels. We do this since any given point
+        may or may not end up in the CharMap, and therefore only need a subset
+        of icons, tags, and labels (depending on each point being in the
+        camera's POV).
+
+        See add_object() for details on what tags and labels represent.
+
+        points  -- a list of 3D points
+        icon    -- a function that takes an index (of point) and returns the
+                    character to draw for it
+        tag     -- a function that takes and index (of point) and returns a
+                    tag for the icon
+        labels  -- a function that takes an index (of point) and returns a
+                    sequence of labels to write for it
+        """
+        pix_pos = self._get_projected_pixels(points)
         for i, x, y in pix_pos:
             self.write_char(x, y, icon(i), tag(i))
-            if label:
-                labels.append((i, x, y))
-        for i, x, y in labels:
-            self.write_label(x, y, label(i))
+        if labels:
+            for i, x, y in pix_pos:
+                for label in labels(i):
+                    self.write_label(x+1, y, *label)
 
-    def add_object(self, point, icon, tag=None, label=None):
-        pix_pos = self.get_projected_pixel(point)
+    def add_object(self, point, icon, tag=None, labels=None):
+        """Add an object to the character map.
+        A tag is an HTML tag (usually the color name) or None for plain text.
+        A label is a tuple of 2 strings: text and aforementioned tag.
+
+        points  -- a 3D point
+        icon    -- the character to draw at that point
+        tag     -- a tag for the icon
+        labels  -- a sequence of labels
+        """
+        pix_pos = self._get_projected_pixel(point)
         if pix_pos is None:
             return
         x, y = pix_pos
         self.write_char(x, y, icon, tag)
-        if label:
-            self.write_label(x, y, label)
+        if labels:
+            for label in labels:
+                self.write_label(x+1, y, *label)
         return True
 
     def add_projection_axes(self):
         labels = ['X+', 'X-', 'Y+', 'Y-', 'Z+', 'Z-']
-        pix_pos = self.get_projected_pixels(AXES_VECTORS)
+        pix_pos = self._get_projected_pixels(AXES_VECTORS)
         for i, x, y in pix_pos:
             self.write_char(x, y, '╬', 'bold')
             self.write_label(x, y, labels[i])
@@ -75,22 +122,22 @@ class CharMap:
         if point is None:
             cx, cy = self.center
         else:
-            pix_pos = self.get_projected_pixel(point)
+            pix_pos = self._get_projected_pixel(point)
             if pix_pos is None:
                 return
             cx, cy = pix_pos
         scoords = (cx, cy-1), (cx, cy+1), (cx-1, cy), (cx+1, cy)  # 2 vertical, 2 horizontal
         dcoords = (cx+1, cy+1), (cx-1, cy-1), (cx-1, cy+1), (cx+1, cy-1)  # 2 left diag, 2 right diag
-        straight = sum(self.check_empty(*c) for c in scoords)
-        diagonal = sum(self.check_empty(*c) for c in dcoords)
+        straight = sum(self._check_empty(*c) for c in scoords)
+        diagonal = sum(self._check_empty(*c) for c in dcoords)
         if diagonal >= straight:
             diag_chars = '\\\\//'
             for i, (cx, cy) in enumerate(dcoords):
-                self.write_char(cx, cy, diag_chars[i], color)
+                self.write_char(cx, cy, diag_chars[i], color, ignore_fail=True)
         else:
             straight_chars = '││──'
             for i, (cx, cy) in enumerate(scoords):
-                self.write_char(cx, cy, straight_chars[i], color)
+                self.write_char(cx, cy, straight_chars[i], color, ignore_fail=True)
 
     def add_prograde_retrograde(self, velocity, show_labels=False, show_speed=False):
         mag = np.linalg.norm(velocity)
@@ -108,8 +155,80 @@ class CharMap:
         self.add_object(-velocity, '+', 'red', ret_label)
         self.add_crosshair(-velocity, color='red')
 
-    def get_projected_pixels(self, points):
-        # Convert 3d position to mercator projection (latitude, longitude)
+    # Writing to map
+    def write_char(self, x, y, char, tag=None, overwrite=False, ignore_fail=False):
+        assert len(char) == 1
+        if tag:
+            char = f'<{tag}>{char}</{tag}>'
+        try:
+            if overwrite or self._check_empty(x, y):
+                self.charmap[y][x] = char
+                return True
+        except IndexError as e:
+            if not ignore_fail:
+                m = f'Trying to write to charmap at {x}, {y}; max at {len(charmap[0])-1}, {len(charmap)-1}.'
+                logger.warning(m)
+                raise IndexError(m)
+        return False
+
+    def write_label(self, x, y, label, tag=None, pad_right=True, allow_offset=True):
+        if pad_right:
+            label = f'{label} '
+        label_size = len(label)
+        normal = self._count_empty_spaces(x, y, label_size)
+        label_fits_normal = normal >= label_size
+        if label_fits_normal:
+            self._insert_label(x, y, label, tag)
+            return
+        if not allow_offset:
+            if normal >= self.minimum_label_size:
+                self._insert_label(x, y, label, tag)
+            return
+        # Check if label fits below
+        below = self._count_empty_spaces(x, y+1, label_size)
+        if below >= label_size:
+            self._insert_label(x, y+1, label, tag)
+            return
+        # Check if label fits above
+        above = self._count_empty_spaces(x, y-1, label_size)
+        if above >= label_size:
+            self._insert_label(x, y-1, label, tag)
+            return
+        # Use location with the most room for label
+        options = np.asarray([above, normal, below])
+        idy = np.argmax(options) - 1
+        if options[idy] >= self.minimum_label_size:
+            self._insert_label(x, y+idy, label, tag)
+
+    def _insert_label(self, x, y, label, tag=None):
+        for i, char in enumerate(label):
+            # Check if next pixel is
+            if x+i+1 >= self.width or self.charmap[y][x+i+1] != ' ':
+                if i > 0 and tag is not None:
+                    self.charmap[y][x+i-1] = f'{self.charmap[y][x+i-1]}</{tag}>'
+                break
+            if i == 0 and tag:
+                char = f'<{tag}>{char}'
+            self.charmap[y][x+i] = char if char != ' ' else WHITESPACE
+        else:
+            if tag is not None:
+                self.charmap[y][x+i] = f'{self.charmap[y][x+i]}</{tag}>'
+
+    def _count_empty_spaces(self, x, y, max_chars=float('inf')):
+        total = 0
+        while self._check_empty(x, y) and total < max_chars:
+            total += 1
+            x += 1
+        return total
+
+    def _check_empty(self, x, y):
+        if 0 <= y < self.height and 0 <= x < self.width:
+            return self.charmap[y][x] == ' '
+        return False
+
+    # Projections
+    def _get_projected_pixels(self, points):
+        """Convert 3D positions to mercator projection (latitude, longitude)"""
         ll_coords = self.camera.get_projected_coords(points)
         # Stretch to aspect ratio and zoom to get pixel coordinates
         pix = ll_coords * [1, CONFIG_DATA['ASPECT_RATIO']] * self.camera.zoom
@@ -129,72 +248,9 @@ class CharMap:
         r = np.asarray(np.round(r), dtype=np.int32)
         return r
 
-    def get_projected_pixel(self, point):
-        pix_pos = self.get_projected_pixels(np.asarray([point]))
+    def _get_projected_pixel(self, point):
+        """Convert a single 3D position to mercator projection (latitude, longitude)"""
+        pix_pos = self._get_projected_pixels(np.asarray([point]))
         if len(pix_pos) == 0:
             return None
         return pix_pos[0, 1:]
-
-    def write_label(self, x, y, label):
-        x += 1  # Offset label to the right of object
-        label = f'{label} '  # Add small whitespace as padding
-        label_size = len(label)
-        normal = self.count_empty_spaces(x, y, label_size)
-        if normal >= label_size:
-            self.insert_label(x, y, label)
-            return
-        below = self.count_empty_spaces(x, y+1, label_size)
-        if below >= label_size:
-            self.insert_label(x, y+1, label)
-            return
-        above = self.count_empty_spaces(x, y-1, label_size)
-        if above >= label_size:
-            self.insert_label(x, y-1, label)
-            return
-        options = [above, normal, below]
-        idy = np.argmax(np.asarray(options)) - 1
-        if options[idy] > self.minimum_label_size:
-            self.insert_label(x, y+idy, label)
-
-    def insert_label(self, x, y, name):
-        for i, char in enumerate(name):
-            if x+i+1 >= self.width or self.charmap[y][x+i+1] != ' ':
-                break
-            self.charmap[y][x+i] = char if char != ' ' else WHITESPACE
-
-    def count_empty_spaces(self, x, y, max_chars=float('inf')):
-        if y >= self.height:
-            return -1
-        total = 0
-        while x < self.width and total < max_chars and self.check_empty(x, y):
-            x += 1
-            total += 1
-        return total
-
-    def check_empty(self, x, y):
-        if 0 <= y < self.height and 0 <= x < self.width:
-            return self.charmap[y][x] == ' '
-        return False
-
-    def write_char(self, x, y, char, tags=None, overwrite=False):
-        assert len(char) == 1
-        if tags:
-            if isinstance(tags, str):
-                char = f'<{tags}>{char}</{tags}>'
-            else:
-                char = self.wrap_tags(char, tags)
-        try:
-            if overwrite or self.check_empty(x, y):
-                self.charmap[y][x] = char
-                return True
-        except IndexError as e:
-            m = f'Trying to write to charmap at {x}, {y}; max at {len(charmap[0])-1}, {len(charmap)-1}.'
-            logger.warning(m)
-            raise IndexError(m)
-        return False
-
-    @staticmethod
-    def wrap_tags(s, tags):
-        for tag in tags:
-            s = f'<{tag}>{s}</{tag}>'
-        return s
